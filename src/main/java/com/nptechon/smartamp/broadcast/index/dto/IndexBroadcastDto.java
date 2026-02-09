b6a2b33de922f720169038aac48cf0d19a601595
zyerin <zyerin5974@gmail.com>
2026-02-09 19:29:57 +0900

    feat: 저장된 음원 인덱스 방송 요청을 위한 IndexBroadcastDto 추가
    - 앰프 ID와 음원 인덱스를 포함한 인덱스 방송 요청 DTO 정의
    - 저장된 음원 재생(인덱스 기반) API 요청 데이터 구조화

---
+++ src/main/java/com/nptechon/smartamp/broadcast/index/dto/IndexBroadcastDto.java
@@ -0,0 +1,11 @@
+package com.nptechon.smartamp.broadcast.index.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class IndexBroadcastDto {
+    private int ampId;
+    private int index;
+}
