84819e411f8627f88b8cd8f5a787c55c370d7ed8
zyerin <zyerin5974@gmail.com>
2026-02-14 20:35:36 +0900

    feat: 직접 방송 요청 VoiceBroadcastRequestDto 생성(repeat 필드 추가로 인해 방식 변경)
    - ampId 단일 파라미터 방식에서 VoiceBroadcastRequestDto(@RequestPart dto) 기반으로 변경
    - 음성 방송 시 반복 횟수(repeat) 전달

---
+++ src/main/java/com/nptechon/smartamp/broadcast/voice/dto/VoiceBroadcastRequestDto.java
@@ -0,0 +1,12 @@
+package com.nptechon.smartamp.broadcast.voice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VoiceBroadcastRequestDto {
+    private int ampId;
+    private int repeat;
+}
