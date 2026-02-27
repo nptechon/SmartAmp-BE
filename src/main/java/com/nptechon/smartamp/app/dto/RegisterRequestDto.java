dd8a1d10cb209e7a8d6f2a5f7e88bd467fa628b4
zyerin <zyerin5974@gmail.com>
2026-02-27 21:34:44 +0900

    feat: 앱 등록 요청용 RegisterRequestDto 추가
    - 앱 최초 등록 비밀번호 전달을 위한 DTO 정의
    - password 필드 구성 및 Lombok 생성자/Getter 적용

---
+++ src/main/java/com/nptechon/smartamp/app/dto/RegisterRequestDto.java
@@ -0,0 +1,12 @@
+package com.nptechon.smartamp.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDto {
+    private String password;
+}
